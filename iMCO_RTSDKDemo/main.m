027501834f69180cc57ecf89ca6e4434d20c8353
Mr.jiang <414816566@qq.com>
2017-07-03 14:53:03 +0800

    Initial Commit

---
+++ iMCO_RTSDKDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iMCO_RTSDKDemo
+//
+//  Created by aimoke on 2017/7/3.
+//  Copyright © 2017年 zhuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ iMCO_RTSDKDemoTests/iMCO_RTSDKDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  iMCO_RTSDKDemoTests.m
+//  iMCO_RTSDKDemoTests
+//
+//  Created by aimoke on 2017/7/3.
+//  Copyright © 2017年 zhuo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iMCO_RTSDKDemoTests : XCTestCase
+
+@end
+
+@implementation iMCO_RTSDKDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
